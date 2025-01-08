ce7cda67ddd22f9c88dd84155544978a9157e2d3
Aman Sharma <asharma8464@gmail.com>
2025-01-08 18:19:44 +0530

    theme changed

--- script.js
+++ script.js
@@ -1,85 +1,75 @@
-let randomNum = (Math.floor(Math.random()*100+1))
-let form = document.querySelector('form')
-let previousGuess = document.querySelector('.guesses')
-let restartGameBtn = document.querySelector('#restart-game')
-let remainGuess = document.querySelector('.lastResult')
-remainGuess.innerText = 10
-let lowOrHi = document.querySelector('.lowOrHi')
-let guessVal = document.querySelector('#guessField')
+let randomNum = Math.floor(Math.random() * 100 + 1);
+let form = document.querySelector("form");
+let previousGuess = document.querySelector(".guesses");
+let restartGameBtn = document.querySelector("#restart-game");
+let remainGuess = document.querySelector(".lastResult");
+remainGuess.innerText = 10;
+let lowOrHi = document.querySelector(".lowOrHi");
+let guessVal = document.querySelector("#guessField");
 let readyToPlay = true;
 
-function letsPlay (){
-   restartGameBtn.style.display = 'none'
-form.addEventListener('submit',function(e){
-  e.preventDefault()
-  if(readyToPlay){
+function letsPlay() {
+  restartGameBtn.style.display = "none";
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (readyToPlay) {
+      guessVal1 = Number(guessVal.value);
 
-   guessVal1 = Number(guessVal.value)
-
-
-  if(guessVal1 === 0 || isNaN(guessVal1) || guessVal1 > 100){
-    lowOrHi.innerText = 'Enter a Valid Number'
-    guessVal.value = ''
-  }
-  else{
-    previousGuess.innerText += `${guessVal1} ,`
-    remainGuess.innerText--
-    if(guessVal1 < randomNum){
-      lowOrHi.innerText = 'Less Than Random Number'
-      guessVal.value = ''
+      if (guessVal1 === 0 || isNaN(guessVal1) || guessVal1 > 100) {
+        lowOrHi.innerText = "Enter a Valid Number";
+        guessVal.value = "";
+      } else {
+        previousGuess.innerText += `${guessVal1} ,`;
+        remainGuess.innerText--;
+        if (guessVal1 < randomNum) {
+          lowOrHi.innerText = "Less Than Random Number";
+          guessVal.value = "";
+        }
+        if (guessVal1 > randomNum) {
+          lowOrHi.innerText = "Greater Than Random Number";
+          guessVal.value = "";
+        }
+        if (guessVal1 === randomNum) {
+          lowOrHi.innerText = ` Correct!!!, Number was ${randomNum} `;
+          guessVal.value = "";
+          endGame();
+        }
+      }
+      if (remainGuess.innerText === "0") {
+        if (guessVal1 === randomNum) {
+          lowOrHi.innerText = ` Correct!!!, Number was ${randomNum} `;
+          endGame();
+        } else {
+          endGame();
+          remainGuess.innerText = 0;
+          lowOrHi.innerText = ` Out Of Turns , Number was ${randomNum} ,Better Luck Next Time.. `;
+          guessVal.value = "";
+        }
+      }
     }
-    if(guessVal1 > randomNum){
-      lowOrHi.innerText = 'Greater Than Random Number'
-      guessVal.value = ''
-    }
-    if(guessVal1 === randomNum){
-      lowOrHi.innerText = ` Correct!!!, Number was ${randomNum} `
-      guessVal.value = ''
-      endGame();
-    }
-    
-  }
-  if(remainGuess.innerText === '0' ){
-    if(guessVal1 === randomNum){
-      lowOrHi.innerText = ` Correct!!!, Number was ${randomNum} `
-        endGame();
-    }
-    else{
-      endGame();
-      remainGuess.innerText = 0
-      lowOrHi.innerText = ` Out Of Turns , Number was ${randomNum} ,Better Luck Next Time.. `
-      guessVal.value = ''
-    }
-
-  }
-}
-})
-
+  });
 }
 
-function endGame(){
-  readyToPlay = false
-  guessVal.setAttribute('disabled','')
+function endGame() {
+  readyToPlay = false;
+  guessVal.setAttribute("disabled", "");
   restartGame();
 }
-function restartGame(){
-  randomNum = (Math.floor(Math.random()*100+1))
-  restartGameBtn.style.display = 'inline-block'
-  restartGameBtn.addEventListener('click',()=>{
-    readyToPlay = true
-    remainGuess.innerText = 10
-  previousGuess.innerText = ''
-  guessVal.removeAttribute('disabled','')
-  hideRestart();
-  })
+function restartGame() {
+  randomNum = Math.floor(Math.random() * 100 + 1);
+  restartGameBtn.style.display = "inline-block";
+  restartGameBtn.addEventListener("click", () => {
+    readyToPlay = true;
+    remainGuess.innerText = 10;
+    previousGuess.innerText = "";
+    guessVal.removeAttribute("disabled", "");
+    hideRestart();
+  });
 }
-function hideRestart(){
-   restartGameBtn.style.display = 'none'
+function hideRestart() {
+  restartGameBtn.style.display = "none";
 }
 letsPlay();
-
-
-
 
 // Another version of javaScript Code !not completed
 
@@ -119,7 +109,6 @@ letsPlay();
 //       // endGame();
 //     }
 //   }
-
 
 // function message (msg){
 //   lowOrHi.innerText = `${msg}`
